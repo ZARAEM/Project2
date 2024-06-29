9e16769df2e03d3c2b4f10c284e1fd477ef69103
Mearaj Ahmed <mma618@nyu.edu>
2024-06-29 06:03:18 -0400

    ships movement, t key, left: AI, cannonball, ball selector, game over, endgame UI

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -10,8 +10,8 @@
 
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
-#define LOG(argument) std::cout << argument << '\n'
 #define GL_GLEXT_PROTOTYPES 1
+#define LOG(argument) std::cout << argument << '\n'
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
@@ -27,28 +27,32 @@
 enum AppStatus { RUNNING, TERMINATED };
 
 // Game Window Size
-constexpr int WINDOW_WIDTH = 640 * 2,
+constexpr int 
+WINDOW_WIDTH = 640 * 2,
 WINDOW_HEIGHT = 480 * 2;
 
 // Background Color from 0-1
-constexpr float BG_RED = (float)255 / 255,
-BG_GREEN = (float)255 / 255,
-BG_BLUE = (float)255 / 255,
-BG_OPACITY = (float)1;
+// #76b6c4 oceanish color I think
+constexpr float 
+BG_RED = (float) 118 / 255,
+BG_GREEN = (float) 182 / 255,
+BG_BLUE = (float) 196 / 255,
+BG_OPACITY = 1.0f;
 
 // Viewport (Basically the Camera)
-constexpr int VIEWPORT_X = 0,
+constexpr int 
+VIEWPORT_X = 0,
 VIEWPORT_Y = 0,
-
 VIEWPORT_WIDTH = WINDOW_WIDTH,
 VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
 // Shader Path
-constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+constexpr char 
+V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
 F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
 // Milliseconds in 1 Second
-constexpr float MILLISECONDS_IN_SECOND = 1000.0;
+constexpr float MILLISECONDS_IN_SECOND = 1000.0f;
 
 // Number of Textures Generated
 constexpr GLint NUMBER_OF_TEXTURES = 2,
@@ -58,50 +62,57 @@ LEVEL_OF_DETAIL = 0,
 TEXTURE_BORDER = 0;
 
 // Path to Sprites
-constexpr char PENGUIN_BLUE_FILEPATH[] = "assets/penguin_blue.png",
-PUFFLE_ORANGE_FILEPATH[] = "assets/puffle_orange.png";
+//Credit: https://kenney.nl/
+//Recolored to red and blue 
+constexpr char
+SHIPONE_FILEPATH[] = "assets/shipOne.png",
+SHIPTWO_FILEPATH[] = "assets/shipTwo.png",
+CANNONBALL_FILEPATH[] = "assets/cannonBall.png";
 
-// Initial Position of Sprites
-constexpr glm::vec3 INIT_POS_PENGUIN = glm::vec3(0.0f, 0.0f, 0.0f),
-INIT_POS_PUFFLE = glm::vec3(0.5f, -0.25f, 0.0f);
+// Initial Position
+constexpr glm::vec3 INIT_POS_SHIPONE = glm::vec3(-4.0f, 0.0f, 0.0f),
+INIT_POS_SHIPTWO = glm::vec3(4.0f, 0.0f, 0.0f);
 
-constexpr glm::vec3 INIT_ROT_PENGUIN = glm::vec3(0.0f, 0.0f, 0.0f),
-INIT_ROT_PUFFLE = glm::vec3(0.0f, 0.0f, 0.0f);
+//constexpr glm::vec3 INIT_ROT_SHIPONE = glm::vec3(0.0f, 0.0f, 0.0f),
+//INIT_ROT_SHIPTWO = glm::vec3(0.0f, 0.0f, 0.0f);
 
-// Initial Scale of Sprites
-constexpr glm::vec3 INIT_SCALE_PENGUIN = glm::vec3(1.0f, 1.0f, 1.0f),
-INIT_SCALE_PUFFLE = glm::vec3(0.5f, 0.5, 0.5f);
-
-constexpr float PENGUIN_TRANSLATION_INCREMENT = 0.1f,
-PENGUIN_ROTATION_INCREMENT = 0.0f,
-PENGUIN_SCALE_INCREMENT = 0.1f;
-
-constexpr float PUFFLE_TRANSLATION_INCREMENT = 0.0f,
-PUFFLE_ROTATION_INCREMENT = 0.3f,
-PUFFLE_SCALE_INCREMENT = -0.05f;
+// Initial Scale
+constexpr glm::vec3 
+INIT_SCALE_SHIPONE = glm::vec3(66.0/128.0f, 113.0/128.0f, 0.0f),
+INIT_SCALE_SHIPTWO = glm::vec3(66.0/128.0f, 113.0/128.0f, 0.0f);
 
 SDL_Window* g_display_window;
 AppStatus g_app_status = RUNNING;
 ShaderProgram g_shader_program = ShaderProgram();
 
 glm::mat4 g_view_matrix,
-g_penguin_matrix,
-g_puffle_matrix,
+g_shipOne_matrix,
+g_shipTwo_matrix,
 g_projection_matrix;
 
 float g_previous_ticks = 0.0f;
 
-glm::vec3 g_penguin_translation = glm::vec3(0.0f, 0.0f, 0.0f),
-g_puffle_translation = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3
+g_shipOne_position = glm::vec3(0.0f, 0.0f, 0.0f),
+g_shipOne_movement = glm::vec3(0.0f, 0.0f, 0.0f),
 
-glm::vec3 g_penguin_rotation = glm::vec3(0.0f, 0.0f, 0.0f),
-g_puffle_rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+g_shipTwo_position = glm::vec3(0.0f, 0.0f, 0.0f),
+g_shipTwo_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
-glm::vec3 g_penguin_scale = glm::vec3(0.0f, 0.0f, 0.0f),
-g_puffle_scale = glm::vec3(0.0f, 0.0f, 0.0f);
+float 
+g_shipOne_speed = 1.0f,
+g_shipTwo_speed = 1.0f;
 
-GLuint g_penguin_texture_id,
-g_puffle_texture_id;
+glm::vec3 g_shipOne_rotation = glm::vec3(0.0f, 0.0f, 0.0f),
+g_shipTwo_rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_shipOne_scale = glm::vec3(0.0f, 0.0f, 0.0f),
+g_shipTwo_scale = glm::vec3(0.0f, 0.0f, 0.0f);
+
+GLuint g_shipOne_texture_id,
+g_shipTwo_texture_id;
+
+bool g_twoPlayerMode = true;
 
 GLuint load_texture(const char* filepath);
 
@@ -132,12 +143,32 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+GLuint load_texture(const char* filepath) {
+    // step 1: loading the image file
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    // step 2: generating and binding a texture id to our image
+    GLuint textureid;
+    glGenTextures(1, &textureid);
+    glBindTexture(GL_TEXTURE_2D, textureid);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    // step 3: setting our texture filter parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // step 4: releasing our file from memory and returning our texture id
+    stbi_image_free(image);
+
+    return textureid;
+}
+
 void initialise()
 {
     // Initialise video and joystick subsystems
     SDL_Init(SDL_INIT_VIDEO);
 
-    g_display_window = SDL_CreateWindow("CLUB PENGUIN ?",
+    g_display_window = SDL_CreateWindow("SHIP PONG ?",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_OPENGL);
@@ -159,8 +190,9 @@ void initialise()
 
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
 
-    g_penguin_matrix = glm::mat4(1.0f);
-    g_puffle_matrix = glm::mat4(1.0f);
+    g_shipOne_matrix = glm::mat4(1.0f);
+    g_shipTwo_matrix = glm::mat4(1.0f);
+
     g_view_matrix = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
@@ -171,8 +203,8 @@ void initialise()
 
     glClearColor(BG_RED, BG_GREEN, BG_BLUE, BG_OPACITY);
 
-    g_penguin_texture_id = load_texture(PENGUIN_BLUE_FILEPATH);
-    g_puffle_texture_id = load_texture(PUFFLE_ORANGE_FILEPATH);
+    g_shipOne_texture_id = load_texture(SHIPONE_FILEPATH);
+    g_shipTwo_texture_id = load_texture(SHIPTWO_FILEPATH);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -180,15 +212,84 @@ void initialise()
 
 void process_input()
 {
+    g_shipOne_movement = glm::vec3(0.0f);
+    g_shipTwo_movement = glm::vec3(0.0f);
+
     SDL_Event event;
+
     while (SDL_PollEvent(&event))
     {
         switch (event.type) {
+
+        // End Game
         case SDL_QUIT:
+            g_app_status = TERMINATED;
+            break;
         case SDL_WINDOWEVENT_CLOSE:
             g_app_status = TERMINATED;
             break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_t:
+                g_twoPlayerMode = !g_twoPlayerMode;
+                break;
+
+            default:
+                break;
+            }
+        default:
+            break;
         }
+    }
+
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_W] && key_state[SDL_SCANCODE_S]) {
+        if (g_twoPlayerMode) {
+            g_shipOne_movement.y = 0.0f;
+        }
+        else {
+            g_shipOne_movement.y = 0.0f;
+        }
+    }
+
+    else if (key_state[SDL_SCANCODE_W] && !key_state[SDL_SCANCODE_S]) {
+        if (g_twoPlayerMode) {
+            g_shipOne_movement.y = 1.0f;
+        }
+        else {
+            g_shipOne_movement.y = 0.0f;
+        }
+    }
+
+    else if (key_state[SDL_SCANCODE_S] && !key_state[SDL_SCANCODE_W]) {
+        if (g_twoPlayerMode) {
+            g_shipOne_movement.y = -1.0f;
+        }
+        else {
+            g_shipOne_movement.y = 0.0f;
+        }
+    }
+
+    if (key_state[SDL_SCANCODE_UP] && key_state[SDL_SCANCODE_DOWN]) {
+        g_shipTwo_movement.y = 0.0f;
+    }
+
+    else if (key_state[SDL_SCANCODE_UP] && !key_state[SDL_SCANCODE_DOWN]) {
+        g_shipTwo_movement.y = 1.0f;
+    }
+
+    else if (key_state[SDL_SCANCODE_DOWN] && !key_state[SDL_SCANCODE_UP]) {
+        g_shipTwo_movement.y = -1.0f;
+    }
+
+    if (glm::length(g_shipOne_movement) > 1.0f) {
+        g_shipOne_movement = glm::normalize(g_shipOne_movement);
+    }
+
+    if (glm::length(g_shipTwo_movement) > 1.0f) {
+        g_shipTwo_movement = glm::normalize(g_shipTwo_movement);
     }
 }
 
@@ -199,25 +300,56 @@ void update()
     float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
 
-    /* Game logic */
-    g_penguin_translation.x += PENGUIN_TRANSLATION_INCREMENT * delta_time;
-    g_penguin_translation.y += PENGUIN_TRANSLATION_INCREMENT * delta_time;
+    g_shipOne_position += g_shipOne_movement * g_shipOne_speed * delta_time;
+    g_shipTwo_position += g_shipTwo_movement * g_shipTwo_speed * delta_time;
 
-    g_puffle_rotation.y += PUFFLE_ROTATION_INCREMENT * delta_time;
+    // Game Logic
+    g_shipOne_matrix = glm::mat4(1.0f); // Reset
+    g_shipOne_matrix = glm::translate(g_shipOne_matrix, INIT_POS_SHIPONE); // xPos
+    g_shipOne_matrix = glm::translate(g_shipOne_matrix, g_shipOne_position); // yPos
 
-    g_penguin_scale += PENGUIN_SCALE_INCREMENT * delta_time;
-    g_puffle_scale += PUFFLE_SCALE_INCREMENT * delta_time;
+    g_shipTwo_matrix = glm::mat4(1.0f); // Reset
+    g_shipTwo_matrix = glm::translate(g_shipTwo_matrix, INIT_POS_SHIPTWO); // xPos
+    g_shipTwo_matrix = glm::translate(g_shipTwo_matrix, g_shipTwo_position); // yPos
 
-    g_penguin_matrix = glm::mat4(1.0f);
-    g_puffle_matrix = glm::mat4(1.0f);
+    /* Top and Bottom of Screen */
+    const float TOP = 3.75f;
+    const float BOTTOM = -1 * TOP;
 
-    g_penguin_matrix = glm::translate(g_penguin_matrix, INIT_POS_PENGUIN + g_penguin_translation);
-    g_penguin_matrix = glm::rotate(g_penguin_matrix, g_penguin_rotation.y, glm::vec3(1.0f, 0.0f, 0.0f));
-    g_penguin_matrix = glm::scale(g_penguin_matrix, INIT_SCALE_PENGUIN + g_penguin_scale);
+    g_shipOne_matrix = glm::scale(g_shipOne_matrix, INIT_SCALE_SHIPONE);
+    g_shipTwo_matrix = glm::scale(g_shipTwo_matrix, INIT_SCALE_SHIPTWO);
 
-    g_puffle_matrix = glm::translate(g_penguin_matrix, INIT_POS_PUFFLE + g_puffle_translation);
-    g_puffle_matrix = glm::rotate(g_puffle_matrix, g_puffle_rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-    g_puffle_matrix = glm::scale(g_puffle_matrix, INIT_SCALE_PUFFLE + g_puffle_scale);
+    // Top and bottom calculated by taking current position and adding/subtractng half of size of ship
+
+    /* Top of shipOne */
+    float shipOne_top = g_shipOne_position.y + INIT_POS_SHIPONE.y + (INIT_SCALE_SHIPONE.y / 2.0f);
+    
+    /* Bottom of shipOne */
+    float shipOne_bottom = g_shipOne_position.y + INIT_POS_SHIPONE.y - (INIT_SCALE_SHIPONE.y / 2.0f);
+
+    /* Top of shipTwo */
+    float shipTwo_top = g_shipTwo_position.y + INIT_POS_SHIPTWO.y + (INIT_SCALE_SHIPTWO.y / 2.0f);
+    
+    /* Bottom of shipTwo */
+    float shipTwo_bottom = g_shipTwo_position.y + INIT_POS_SHIPTWO.y - (INIT_SCALE_SHIPTWO.y / 2.0f);
+
+    // If ships are above or below top or bottom respectively, move them to topmost/bottomost position
+    
+    if (shipOne_top > TOP) {
+        g_shipOne_position.y = TOP - INIT_POS_SHIPONE.y - (INIT_SCALE_SHIPONE.y / 2.0f);
+    }
+
+    if (shipOne_bottom < BOTTOM) {
+        g_shipOne_position.y = BOTTOM - INIT_POS_SHIPONE.y + (INIT_SCALE_SHIPONE.y / 2.0f);
+    }
+
+    if (shipTwo_top > TOP) {
+        g_shipTwo_position.y = TOP - INIT_POS_SHIPTWO.y - (INIT_SCALE_SHIPTWO.y / 2.0f);
+    }
+
+    if (shipTwo_bottom < BOTTOM) {
+        g_shipTwo_position.y = BOTTOM - INIT_POS_SHIPTWO.y + (INIT_SCALE_SHIPTWO.y / 2.0f);
+    }
 
 }
 
@@ -225,7 +357,7 @@ void draw_object(glm::mat4& object_g_model_matrix, GLuint& object_texture_id)
 {
     g_shader_program.set_model_matrix(object_g_model_matrix);
     glBindTexture(GL_TEXTURE_2D, object_texture_id);
-    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so use 6, not 3
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
 void render()
@@ -255,8 +387,8 @@ void render()
     glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
     // Bind texture
-    draw_object(g_penguin_matrix, g_penguin_texture_id);
-    draw_object(g_puffle_matrix, g_puffle_texture_id);
+    draw_object(g_shipOne_matrix, g_shipOne_texture_id);
+    draw_object(g_shipTwo_matrix, g_shipTwo_texture_id);
 
     // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
@@ -266,23 +398,3 @@ void render()
 }
 
 void shutdown() { SDL_Quit(); }
-
-GLuint load_texture(const char* filepath) {
-    // step 1: loading the image file
-    int width, height, number_of_components;
-    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
-
-    // step 2: generating and binding a texture id to our image
-    GLuint textureid;
-    glGenTextures(1, &textureid);
-    glBindTexture(GL_TEXTURE_2D, textureid);
-    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
-
-    // step 3: setting our texture filter parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-    // step 4: releasing our file from memory and returning our texture id
-    stbi_image_free(image);
-
-    return textureid;
-}
